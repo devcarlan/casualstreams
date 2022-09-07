7ffcb0da5763e8132ae4c7261576dc0d4d411fa2
devcarlan <devcarlan@gmail.com>
2022-09-07 19:16:32 -0400

    working on navigation

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,13 +1,32 @@
 import Link from 'next/link'
 
 export const Nav = () => {
+  const links = [
+    { name: 'STREAMS', url: '/' },
+    { name: 'ABOUT', url: '/' },
+    { name: 'CONTACT', url: '/' },
+  ]
   return (
     <div className='inline-flex min-w-full justify-between items-center md:py-4'>
       <div className='ml-4'>
         <Link href='/'>
-          <p className='text-2xl'>Casual Streams</p>
+          <a>
+            <p className='text-2xl'>Casual Streams</p>
+          </a>
         </Link>
       </div>
+      <nav className='md:flex'>
+        <ul className='md:inline-flex md:justify-between'>
+          {links.map((link, i) => (
+            <li
+              key={i}
+              className='mx-4 cursor-pointer text-zinc-800 hover:text-emerald-600'
+            >
+              <Link href={link.url}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   )
 }
