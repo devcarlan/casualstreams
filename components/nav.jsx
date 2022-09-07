f3fa975dd81c262da42311f2ba2733890c37f557
devcarlan <devcarlan@gmail.com>
2022-09-07 19:10:19 -0400

    working on navigation component

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,3 +1,13 @@
+import Link from 'next/link'
+
 export const Nav = () => {
-  return <div>Navigation</div>
+  return (
+    <div className='inline-flex min-w-full justify-between items-center md:py-4'>
+      <div className='ml-4'>
+        <Link href='/'>
+          <p className='text-2xl'>Casual Streams</p>
+        </Link>
+      </div>
+    </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Nav } from '../components/nav'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <div className='text-3xl'>Casual Streams</div>
+  return <Nav />
 }
