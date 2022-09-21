b88da7a3b57ed050e763f5760f26bac06dacd80f
devcarlan <devcarlan@gmail.com>
2022-09-21 17:08:24 -0400

    working on getting access token from twitch

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Nav } from '../components/nav'
+import syncStreams from '../lib/syncstreams'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <div>Home Page</div>
+  syncStreams()
+  return <div>HOME</div>
 }
