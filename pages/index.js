7b01ead941e0193c11841c19fd7757a77b8aada7
devcarlan <devcarlan@gmail.com>
2022-09-23 17:48:22 -0400

    adding ability to get twitch streams

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,17 @@ import { Nav } from '../components/nav'
 import syncStreams from '../lib/syncstreams'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  syncStreams()
+export async function getServerSideProps() {
+  const client = process.env.CLIENT
+  const secret = process.env.SECRET
+  const casuals = await syncStreams(client, secret)
+
+  return {
+    props: { casuals },
+  }
+}
+
+export default function Home({ casuals }) {
+  console.log(casuals)
   return <div>HOME</div>
 }
